b3133d1996751cbd33c7b18b12e09794db215c8f
Jevgeni Prostsenko <jevgpro@users.noreply.github.com>
2018-09-10 10:39:52 +0300

    html muudetud php-ks

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jevgeni Prostsenko, õpetöö</title>
+</head>
+<body bgcolor="#808080" text="#FA8E47">
+	<h1 align="center">Siin on Jevgeni Prostsenko õpetööd</h1>
+	<p>Siin on minu <a href="http://www.tlu.ee" target="_blank"> TLÜ</a> õpetöö raames valminud veebilehed. Nende ei oma mingit sügavat sisu ja nende kooperimine ei oma mõtet. </p>
+	<p>Tervist, mina olen 1 kursuse TLÜ informaatika student. Enne õppimist mul oli väga meeldiv proovida programmeerida ning 3D animeerida. Selleks ma kasutasin Unity 3D. See oli väga põnev ja huvitav minu jaoks :)</p>
+	<!-- <img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_2.jpg" alt="TLÜ Terra õppehoone"> -->
+	<img src="../../tlu_terra_600x400_2.jpg" alt="TLÜ Terra õppehoone">
+	<p> Mul on sõber, kes ka teeb <a href="../../../~vadizig/index.html">veebi</a>.</p>
+</body>
+</html>
